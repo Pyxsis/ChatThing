7f41a2590d9ff3369c85fb7383f571d518f6c545
Reparo / Joey <joerthecoder@gmail.com>
2017-02-26 14:08:55 +0000

    Chat delay added

--- src/me/reparo/chatthing/events/ChatEvent.java
+++ src/me/reparo/chatthing/events/ChatEvent.java
@@ -1,15 +1,17 @@
 package me.reparo.chatthing.events;
 
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import me.reparo.chatthing.ChatThing;
 import me.reparo.chatthing.Util;
 
 public class ChatEvent implements Listener {
-	
-	@EventHandler
+
+	@EventHandler 
 	public void chat(AsyncPlayerChatEvent e) {
 		/*
 		 * Chat lock
@@ -17,6 +19,22 @@ public class ChatEvent implements Listener {
 		if(Util.chatlocked && !e.getPlayer().hasPermission("chatthing.chatlock.bypass") && Util.isEnabled("chatlock")) {
 			e.setCancelled(true);
 			e.getPlayer().sendMessage(Util.chatlockenablederrormessage(e.getPlayer()));
+		}
+		/*
+		 *  Chat Delay (anti spam)
+		 */
+		if(Util.isEnabled("chatdelay") && !e.getPlayer().hasPermission("chatthing.chatdelay.bypass")) {
+			if(Util.delayedchat.contains(e.getPlayer())) {
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(Util.delaystoppingchatmsg(e.getPlayer()));
+			} else {
+				Util.delayedchat.add(e.getPlayer());
+				Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(ChatThing.ct, new Runnable() {
+					public void run() {
+						Util.delayedchat.remove(e.getPlayer());
+					}
+				}, Util.chatdelay*20);
+			}
 		}
 	}
 	
